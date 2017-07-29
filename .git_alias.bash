2bb098ed6eee0bd6c59f9a558d956081c8d02802
Yongfu Li <liyongfu.sg@gmail.com>
2017-07-29 20:29:09 +0800

    add configuration files

---
+++ .git_alias.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias gst="git status "
+alias gc="git commit -m "
+alias ga="git add "
+alias gr="git rm "
+alias gl="git lola"
+alias gcm="git checkout master"
+alias gco="git checkout"
+alias gp="git pull"
+alias gb="git branch"
